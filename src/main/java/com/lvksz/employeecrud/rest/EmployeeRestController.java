c14eead99573ad9b01055f994953185979ee82f9
lvksz <lukaszgrechv@gmail.com>
2024-02-01 22:46:16 +0100

    Security and RestController are unavailable now. Created new MVC Controller and Thymeleaf page with Bootstrap CSS to display list of employees.

--- src/main/java/com/lvksz/employeecrud/rest/EmployeeRestController.java
+++ src/main/java/com/lvksz/employeecrud/rest/EmployeeRestController.java
@@ -1,63 +1,56 @@
 package com.lvksz.employeecrud.rest;
 
-import com.lvksz.employeecrud.entity.Employee;
-import com.lvksz.employeecrud.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+//@RestController
+//@RequestMapping("/api")
+//public class EmployeeRestController {
 
-import java.util.List;
-
-@RestController
-@RequestMapping("/api")
-public class EmployeeRestController {
-
-    private EmployeeService employeeService;
-
-    @Autowired
-    public EmployeeRestController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
-    }
-
-    @GetMapping("/employee")
-    public List<Employee> findAll(){
-        return employeeService.findAll();
-    }
-
-    @GetMapping("/employee/{employeeId}")
-    public Employee getSingleEmployee(@PathVariable int employeeId){
-
-        Employee employee = employeeService.findById(employeeId);
-
-        if (employee == null){
-            throw new RuntimeException("Employee id not found - " + employeeId);
-        }
-
-        return employee;
-    }
-
-    @PostMapping("/employee")
-    public Employee addEmployee(@RequestBody Employee employee){
-        employee.setId(0);
-        return employeeService.save(employee);
-    }
-
-    @PutMapping("/employee")
-    public Employee updateEmployee(@RequestBody Employee employee){
-        return employeeService.save(employee);
-    }
-
-    @DeleteMapping("/employee/{employeeId}")
-    public String deleteEmployee(@PathVariable int employeeId){
-
-        Employee employee = employeeService.findById(employeeId);
-
-        if (employee == null){
-            throw new RuntimeException("Employee id not found - " + employeeId);
-        }
-        employeeService.deleteById(employeeId);
-
-        return "Deleted employee id - " + employeeId;
-    }
+//    private EmployeeService employeeService;
+//
+//    @Autowired
+//    public EmployeeRestController(EmployeeService employeeService) {
+//        this.employeeService = employeeService;
+//    }
+//
+//    @GetMapping("/employee")
+//    public List<Employee> findAll(){
+//        return employeeService.findAll();
+//    }
+//
+//    @GetMapping("/employee/{employeeId}")
+//    public Employee getSingleEmployee(@PathVariable int employeeId){
+//
+//        Employee employee = employeeService.findById(employeeId);
+//
+//        if (employee == null){
+//            throw new RuntimeException("Employee id not found - " + employeeId);
+//        }
+//
+//        return employee;
+//    }
+//
+//    @PostMapping("/employee")
+//    public Employee addEmployee(@RequestBody Employee employee){
+//        employee.setId(0);
+//        return employeeService.save(employee);
+//    }
+//
+//    @PutMapping("/employee")
+//    public Employee updateEmployee(@RequestBody Employee employee){
+//        return employeeService.save(employee);
+//    }
+//
+//    @DeleteMapping("/employee/{employeeId}")
+//    public String deleteEmployee(@PathVariable int employeeId){
+//
+//        Employee employee = employeeService.findById(employeeId);
+//
+//        if (employee == null){
+//            throw new RuntimeException("Employee id not found - " + employeeId);
+//        }
+//        employeeService.deleteById(employeeId);
+//
+//        return "Deleted employee id - " + employeeId;
+//    }
 
 
-}
+//}
--- src/main/java/com/lvksz/employeecrud/security/SecurityConfig.java
+++ src/main/java/com/lvksz/employeecrud/security/SecurityConfig.java
@@ -1,51 +1,41 @@
 package com.lvksz.employeecrud.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
-
-    @Bean
-    public UserDetailsManager userDetailsManager(DataSource dataSource){
-        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-
-        jdbcUserDetailsManager.setUsersByUsernameQuery(
-                "SELECT user_id, pw, active FROM members WHERE user_id = ?");
-
-        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
-                "SELECT user_id, role FROM roles WHERE user_id = ?");
-
-        return jdbcUserDetailsManager;
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http.authorizeHttpRequests(configurer ->
-                configurer
-                        .requestMatchers(HttpMethod.GET, "api/employee").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.GET, "api/employee/**").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.PUT, "api/employee").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.POST, "api/employee").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.DELETE, "api/employee/**").hasRole("ADMIN")
-        );
-
-        http.httpBasic(Customizer.withDefaults());
-
-        http.csrf(csrf -> csrf.disable());
-
-        return http.build();
-    }
+//    @Bean
+//    public UserDetailsManager userDetailsManager(DataSource dataSource){
+//        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+//
+//        jdbcUserDetailsManager.setUsersByUsernameQuery(
+//                "SELECT user_id, pw, active FROM members WHERE user_id = ?");
+//
+//        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+//                "SELECT user_id, role FROM roles WHERE user_id = ?");
+//
+//        return jdbcUserDetailsManager;
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+//        http.authorizeHttpRequests(configurer ->
+//                configurer
+//                        .requestMatchers(HttpMethod.GET, "api/employee").hasRole("EMPLOYEE")
+//                        .requestMatchers(HttpMethod.GET, "api/employee/**").hasRole("EMPLOYEE")
+//                        .requestMatchers(HttpMethod.PUT, "api/employee").hasRole("MANAGER")
+//                        .requestMatchers(HttpMethod.POST, "api/employee").hasRole("MANAGER")
+//                        .requestMatchers(HttpMethod.DELETE, "api/employee/**").hasRole("ADMIN")
+//        );
+//
+//        http.httpBasic(Customizer.withDefaults());
+//
+//        http.csrf(csrf -> csrf.disable());
+//
+//        return http.build();
+//    }
 
 //    @Bean
 //    public InMemoryUserDetailsManager userDetailsManager(){
